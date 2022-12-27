bea15ff9c1e60c2a8dcbbda163c376e100802f78
Rosbel <rosbelg15@gmail.com>
2022-12-27 14:21:35 -0300

    agrego conversor de texto normal a texto en abecedario simbolo/numerico

--- Alpha.js
+++ Alpha.js
@@ -1,1 +1,16 @@
-let abc=require("letras.js");
+const abc=require("./letras");
+
+function convertir(texto){
+    let textoTraducido= ""
+    for(let i=0;i< texto.length;i++){
+        if(texto[i]===" "){
+            textoTraducido= textoTraducido + " "
+        } else {
+        textoTraducido = textoTraducido + abc[texto[i].toLowerCase()]
+    }};
+    return textoTraducido
+}
+
+let hola="feli quiero blockazo"
+console.log(hola)
+console.log(convertir(hola))
--- letras.js
+++ letras.js
@@ -26,4 +26,5 @@ const abc = {
   y: "(",
   z: ")",
 };
-module_exports=abc
+
+module.exports=abc
